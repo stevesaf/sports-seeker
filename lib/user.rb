373c511b7ed6deea97b3857e32b7817faa727e83
kenNg1 <kng1230@hotmail.com>
2017-04-19 17:41:41 +0800

    updated list of accepted events on homepage

--- lib/user.rb
+++ lib/user.rb
@@ -5,11 +5,10 @@ class User < ActiveRecord::Base
 
   has_many(:user2, :class_name => 'Friend', :foreign_key => 'user2_id')
 
-  has_many(:histories, dependent: :destroy)
-  has_many(:events, through: :histories)
-
-  has_many(:pendings, dependent: :destroy)
-  has_many(:events, through: :pendings)
+  has_many(:event_users, dependent: :destroy)
+  has_many(:attendee, :class_name => 'EventUser', :foreign_key => 'attendee_id')
+  has_many(:sender, :class_name => 'EventUser', :foreign_key => 'sender_id')
+  has_many(:events, through: :event_users)
 
   has_many (:events)
 
@@ -22,4 +21,15 @@ class User < ActiveRecord::Base
       User.find(id)
     end
   end
+
+  def find_events()
+    user2_list = DB.exec("SELECT friends.user2_id FROM friends WHERE user1_id = #{self.id}")
+    user1_list = DB.exec("SELECT friends.user1_id FROM friends WHERE user2_id = #{self.id}")
+    id2 = user2_list.map {|friend| friend['user2_id']}
+    id1 = user1_list.map {|friend| friend['user1_id']}
+    id2.concat(id1).map do |id|
+      User.find(id)
+    end
+  end
+
 end
