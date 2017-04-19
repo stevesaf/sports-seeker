21ca508dcd3dc929a015268683f9c158c7969d0e
dxsfung <davxsfung@gmail.com>
2017-04-19 10:13:27 +0800

    updated event.erb

--- db/migrate/20170418090037_rename_friends_column.rb
+++ db/migrate/20170418090037_rename_friends_column.rb
@@ -1,4 +1,4 @@
-class RenameFriendsColumn < ActiveRecord::Migration[5.0]
+class RenameFriendsColumn < ActiveRecord::Migration
   def change
     rename_column(:friends, :user1, :user1_id)
     rename_column(:friends, :user2, :user2_id)
